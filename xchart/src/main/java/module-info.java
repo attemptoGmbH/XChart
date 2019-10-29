d5fbbe2cf51cac780a6ba5f3d48385f3eb8096ee
Heinz van Pee <heinz.vanpee@attempto.eu>
2019-10-29 08:37:00 +0100

    Compile for Java 1.8
    removed module-info.java

--- xchart/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module xchart {
-	exports org.knowm.xchart;
-	exports org.knowm.xchart.style;
-	exports org.knowm.xchart.style.colors;
-	exports org.knowm.xchart.style.lines;
-	exports org.knowm.xchart.style.markers;
-	requires java.desktop;
-	requires VectorGraphics2D;
-}
