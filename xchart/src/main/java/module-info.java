5857ffd34ebb7bde6f5212d7da8ce43a42884437
Heinz van Pee <heinz.vanpee@attempto.eu>
2019-10-30 15:40:19 +0100

    Revert "Compile for Java 1.8"
    This reverts commit d5fbbe2cf51cac780a6ba5f3d48385f3eb8096ee.

---
+++ xchart/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module xchart {
+	exports org.knowm.xchart;
+	exports org.knowm.xchart.style;
+	exports org.knowm.xchart.style.colors;
+	exports org.knowm.xchart.style.lines;
+	exports org.knowm.xchart.style.markers;
+	requires java.desktop;
+	requires VectorGraphics2D;
+}
